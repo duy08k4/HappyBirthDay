845200581638565e1d4851e23d445a49d05e1446
cao161 <120444178+cao161@users.noreply.github.com>
2023-07-07 16:00:57 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const contentLetterSrart_actived = "Hãy nói gì đó ở đây trước khi người ấy mở bức thư nhé." //Lời mở đầu cho bức thư
-const mainContentLetter = "Gửi lời nhắn nhủ đến người bạn bạn yêu thương." //Nội dung của bức thư
+const contentLetterSrart_actived = "Hi,người bạn thân nhất của t." //Lời mở đầu cho bức thư
+const mainContentLetter = "Sinh nhật vui vẻ nhó, sang tuổi mới xinh đẹp hơn, học giỏi hơn và luôn thành công trong cuộc sống.HT❤." //Nội dung của bức thư
 
 // Gắn 1 đường link ảnh bất kì
 let imgStart = document.querySelector(".myAI"); //Hình ảnh xuất hiện trong lời mở đầu của bức thư
